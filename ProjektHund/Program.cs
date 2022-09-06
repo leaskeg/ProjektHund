7cc30d2fe957f34f28655ae787d169bb0ff6698e
Jens Bach <leaskegaming@gmail.com>
2022-09-06 09:22:17 +0200

    Add project files.

---
+++ ProjektHund/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektHund
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
