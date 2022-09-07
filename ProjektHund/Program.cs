76769e5eecc4f1a03c3608edf1284f626e09f191
Jens Bach <leaskegaming@gmail.com>
2022-09-07 15:18:25 +0200

    e

--- ProjektHund/Program.cs
+++ ProjektHund/Program.cs
@@ -7,15 +7,6 @@ namespace ProjektHund
     {
         static void Main(string[] args)
         {
-            {
-                string data;
-                FileStream fsSource = new FileStream("D:\\HundeData2.csv", FileMode.Open, FileAccess.Read);
-                using (StreamReader sr = new StreamReader(fsSource))
-                {
-                    data = sr.ReadToEnd();
-                }
-                Console.WriteLine(data);
-            }
                 Dogs dogs = new Dogs();
                 dogs.Menu();
         }
