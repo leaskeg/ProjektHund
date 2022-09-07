70250827680a80b1482225869c8b732f2c137394
Jens Bach <leaskegaming@gmail.com>
2022-09-07 11:59:46 +0200

    New Infrastructure

--- ProjektHund/Program.cs
+++ ProjektHund/Program.cs
@@ -1,11 +1,23 @@
-﻿namespace ProjektHund
+﻿using System.IO;
+
+namespace ProjektHund
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ShowMenu showMenu = new ShowMenu();
-            showMenu.Menu();
+            {
+                string data;
+                FileStream fsSource = new FileStream("D:\\HundeData2.csv", FileMode.Open, FileAccess.Read);
+                using (StreamReader sr = new StreamReader(fsSource))
+                {
+                    data = sr.ReadToEnd();
+                }
+                Console.WriteLine(data);
+            }
+                Dogs dogs = new Dogs();
+                dogs.Menu();
         }
     }
 }
